b369273fa9d4f043fb9bb1558d2e55439d654c6d
Mykola Buhaiov <gulivero1773@icloud.com>
2023-11-23 13:54:18 +0200

    feat: add protocol for service

---
+++ Sources/FSGetAppStatus/FSGetAppStatusServiceable.swift
@@ -0,0 +1,35 @@
+//
+//  FSGetAppStatusServiceable.swift
+//
+//
+//  Created by Mykola Buhaiov on 23.11.2023.
+//  Copyright © 2023 Freedom Space LLC
+//  All rights reserved: http://opensource.org/licenses/MIT
+//
+
+import FluentPostgresDriver
+import Fluent
+import Vapor
+import Redis
+
+public protocol FSGetAppStatusServiceable {
+    func getRedisStatus() -> EventLoopFuture<String>
+
+    func getPostgresStatus() -> EventLoopFuture<(String, String, HTTPResponseStatus)>
+
+    func getMongoDBStatus(host: String, port: String) -> EventLoopFuture<(String, HTTPResponseStatus)>
+
+    func getRedisStatusAsync() async -> String
+
+    func getPostgresStatusAsync() async -> (String, String, HTTPResponseStatus)
+
+    func getMongoDBStatusAsync(host: String, port: String) async -> (String, HTTPResponseStatus)
+
+    func applicationLaunchTime()
+
+    func applicationUpTime() -> Double
+
+    func applicationLaunchDate()
+
+    func applicationUpDate() -> String
+}
